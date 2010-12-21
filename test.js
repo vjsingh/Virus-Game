397b876cca7e7636a0b8fdb501db6d4c065938f1
jonahkagan <jonahkagan@gmail.com>
2010-12-20 19:16:14 -0800

    working on test

--- test.js
+++ test.js
@@ -2,57 +2,49 @@
 // which gets passed in a processing instance p
 var pCode = function(p) {
 
-    var objs = [];
+    var g;
 
-    // built in functions are attributes of the processing instance
     p.setup = function() {
         p.size(500, 500);
 
-        // the constructor for the PVector class is also an attribute of p
-        // (it's useful to glance at the processing.js code for stuff like this)
-        var v = new p.PVector();
-        //alert(v === null); // should be false
-
-        // though we can put js in java code, we can't put java in js
-        // PVector v2 = new PVector(); // doesn't work
-
+        g = game(p); 
 
         // make some test particles
-        objs.push(particle(p, {
+        g.add_object(particle(p, {
             pos: new p.PVector(100, 100),
             vel: new p.PVector(.2, .2)
         }));
 
-        objs.push(particle(p, {
+        g.add_object(particle(p, {
             pos: new p.PVector(300, 100),
             vel: new p.PVector(-.2, .2)
         }));
 
-        objs.push(particle(p, {
+        g.add_object(particle(p, {
             pos: new p.PVector(100, 300),
             vel: new p.PVector(.2, -.2)
         }));
 
-        objs.push(cell(p, {
+        g.add_object(cell(p, {
             pos: new p.PVector(200, 200),
             state: "alive"
         }));
 
-        objs.push(cell(p, {
+        g.add_object(cell(p, {
             pos: new p.PVector(250, 200),
             state: "infected"
         }));
 
-        objs.push(cell(p, {
+        g.add_object(cell(p, {
             pos: new p.PVector(150, 200),
             state: "dead"
         }));
 
-        objs.push(floater(p, {
+        g.add_object(floater(p, {
             pos: new p.PVector(400, 200)
         }));
 
-        objs.push(wall_cell(p, {
+        g.add_object(wall_cell(p, {
             pos: new p.PVector(400, 100)
         }));
 
@@ -61,13 +53,9 @@ var pCode = function(p) {
     // draw is called repeatedly
     p.draw = function() {
         p.background(200);
-        p.fill(0);
-        p.text("testing", p.width/2, p.height/2);
 
-        for (var i=0; i<objs.length; i++) {
-            objs[i].update();
-            objs[i].draw();
-        }
+        g.update_all();
+        g.draw_all();
     };
 }
 
