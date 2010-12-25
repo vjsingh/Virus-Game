56bb99d35e33183304ff5374ae73ab12bb1714ac
vjsingh <varun101@gmail.com>
2010-12-24 19:02:42 -0500

    forgot to add a file

---
+++ num_status_obj.js
@@ -0,0 +1,50 @@
+// Generic wrapper for a status that includes text and a num
+// Provides draw, incr, and set_num
+// spec:
+//		pos : center pos of text
+//		text : Will display "<text> <num>"
+//		num : num to start at
+
+var num_status_obj = function(p, spec) {
+	
+    // obj to return
+    var obj = {};
+
+    // --- private variables ---
+
+	var number = spec.num;
+	var txt = spec.text;
+	var rect = rectangle(p, { // Really just a text obj
+		pos : spec.pos,
+		width : 0,
+		height : 0,
+		text : txt + " " + number //same as get_obj_text
+	})
+	
+	// --- private methods
+	
+	var get_obj_text = function() {
+		return txt + " " + number;
+	}
+	
+	var update = function(n) {
+		number = n;
+		rect.update_text(get_obj_text());
+	}
+	
+    // --- public methods --- 
+
+	obj.draw = function() {
+        rect.draw();
+	};
+	
+	obj.incr = function(n) {
+		update(number + n);
+	};
+	
+	obj.set_num = function(n) {
+		update(n);
+	}
+	
+    return obj;
+};
