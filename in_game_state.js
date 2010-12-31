a7e5f5ac6205f5d3dfe44311dbebc039bf724df5
jonahkagan <jonahkagan@gmail.com>
2010-12-30 23:19:10 -0800

    fixed offset walls bug

--- in_game_state.js
+++ in_game_state.js
@@ -785,7 +785,12 @@ var in_game_state = function (p, previous_state) {
         var add_one = function(rightmost) {
             // if the rightmost has entered the screen 
             if (!rightmost.is_off_right()) {
-                var new_spec = random_from(wall_specs);
+                var wall_spec = random_from(wall_specs);
+                // need to make a new copy
+                var new_spec = {
+                    width: wall_spec.width,
+                    height: wall_spec.height
+                };
 
                 var x = rightmost.get_pos().x
                     + rightmost.get_width()/2
@@ -796,6 +801,10 @@ var in_game_state = function (p, previous_state) {
                 // switch if it's a bottom wall
                 if (rightmost === rightmost_btm) {
                     y = p.height - y;
+                    new_spec.is_top = false;
+                }
+                else {
+                    new_spec.is_top = true;
                 }
                 new_spec.pos = new p.PVector(x, y);
 
@@ -850,7 +859,7 @@ var in_game_state = function (p, previous_state) {
                	f(game_objects[i][j]);
             }
         }
-	}
+	};
 
     // applies a function to every object
     // on the specified rendering level
--- wall_segment.js
+++ wall_segment.js
@@ -2,7 +2,7 @@
 // later can include which image/shape to use
 // MUST HAVE WIDTH AND HEIGHT SPECIFIED
 var wall_specs = [
-    {}
+    { width: 100, height: 40 }
     //{ width: 100, height: 30, fill: 0 },
     //{ width: 200, height: 30, fill: 50 },
     //{ width: 50, height: 30, fill: 100 },
@@ -13,9 +13,11 @@ var wall_specs = [
 // --- inherits from game_object
 // spec:
 //  game_object spec
+//  boolean is_top = true if it is on the top of the screen
 // 
 // Each wall segment must match up with the other
 // wall segments at each end
+// Segment images should be formatted for the bottom wall
 
 var wall_segment = function(p, spec) {
 
@@ -27,6 +29,7 @@ var wall_segment = function(p, spec) {
     spec.width = spec.width || 60;
     spec.height = spec.height || 60;
     spec.fill = spec.fill || 150;
+    spec.mode = p.CENTER;
 
     // obj to return
     var obj = game_object(p, spec);
@@ -37,8 +40,6 @@ var wall_segment = function(p, spec) {
 
     // --- private variables ---
 
-    var alive = true;
-
     // --- public methods --- 
 
     // implementing game_object interface
@@ -47,21 +48,33 @@ var wall_segment = function(p, spec) {
 
     // (flat rect for now)
     obj.draw = function() {	
+        p.pushMatrix();
+
         var pos = obj.get_pos();
         p.shapeMode(obj.mode);
         // all diff colors for testing
-        p.fill(spec.fill);
+        p.fill(0);//spec.fill);
         p.noStroke();
+
         var w = obj.get_width();
         var h = obj.get_height();
-        //p.rect(pos.x-w/2, pos.y-h/2, w, h);
-        p.image(wall_shape, pos.x-w/2, pos.y-h/2, w, h); 
+
+        p.translate(pos.x, pos.y);
+
+        if (spec.is_top) {
+            p.rotate(p.PI);
+        }
+            
+        //p.rect(-w/2, -h/2, w, h);
+        p.imageMode(obj.get_mode());
+        p.image(wall_shape, 0, 0, w, h); 
 		//draw(canvas.getContext('2d'));
 		//var d = new Date();
 		//console.log(d.getMilliseconds());
 		//canvas.getContext('2d').drawSvg("images/virusFinal.svg", 300, 10, 20, 20);
 		//console.log(d.getMilliseconds());
 		
+        p.popMatrix();
     };
 
     obj.draw_circle = function() {};
