808a50a27c28016cafae20ee3bd8278d5ca7f199
jonahkagan <jonahkagan@gmail.com>
2011-01-03 19:55:55 -0800

    added preloading function

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // all the processing code goes in the function
 // which gets passed in a processing instance p
-var pCode = function(p) {
+var p_code = function(p) {
 
     var g, t1, c1, c2;
 
@@ -20,66 +20,6 @@ var pCode = function(p) {
 		g = state_manager(p);
 		
 		p.frameRate(30);
-        //g = in_game_state(p, null); 
-
-        /*
-        g.add_object(particle(p, {
-            pos: new p.PVector(50, 100),
-            vel: new p.PVector(.2, .2)
-        }));
-
-        g.add_object(particle(p, {
-            pos: new p.PVector(300, 100),
-            vel: new p.PVector(-.2, .2)
-        }));
-
-        g.add_object(particle(p, {
-            pos: new p.PVector(300, 300),
-            vel: new p.PVector(.2, -.2)
-        }));
-
-        c1 = cell(p, {
-            pos: new p.PVector(200, 200),
-            state: "alive"
-        });
-        g.add_object(c1);
-
-        c2 = cell(p, {
-            pos: new p.PVector(100, 400),
-            state: "alive"
-        });
-        g.add_object(c2);
-
-        g.add_object(cell(p, {
-            pos: new p.PVector(250, 200),
-            state: "alive"
-        }));
-
-        g.add_object(cell(p, {
-            pos: new p.PVector(150, 200),
-            state: "alive"
-        }));
-
-        g.add_object(floater(p, {
-            pos: new p.PVector(400, 250)
-        }));
-
-        g.add_object(wall_cell(p, {
-            pos: new p.PVector(400, 100)
-        }));
-
-        g.add_object(empty_cell(p, {
-            pos: new p.PVector(400, 200)
-        }));
-
-        t1 = tkiller(p, {
-            pos: new p.PVector(400, 400),
-            target: c1
-        });
-        g.add_object(t1);
-
-        //setTimeout(function() { t1.set_target(c2); }, 9000);
-        */
     };
 
 	update_counter = 0;
@@ -101,13 +41,15 @@ var pCode = function(p) {
 	p.mousePressed = function() {
 		g.mouse_click(p.mouseX, p.mouseY);
 	};
-}
+};
 
 
 // attaches the processing code to the canvas
 // note that we need to do this AFTER the canvas element is created
 var canvas = document.getElementById("test_canvas");
-var sketch = new Processing.Sketch(pCode);
+var sketch = new Processing.Sketch(p_code);
+// this will preload and cache images
+preload_images(sketch);
 sketch.options.isTransparent = true;
 var pInstance = new Processing(canvas, sketch);
 
