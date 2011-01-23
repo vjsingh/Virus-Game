8f3f53988cee664579efc172d7f8f3d6f47b7cf2
csadmin <csadmin@virus-game.cs.brown.edu>
2011-01-23 06:21:38 -0500

    added score posting

--- scores/scores.js
+++ scores/scores.js
@@ -6,11 +6,14 @@ var get_scores = function(callback) {
 
 // interprets json and displays it
 var display_scores = function(data) {
+    $("#scores").empty();
     var scores = jQuery.parseJSON(data);
     //console.log(scores);
+    // scores is an array of row objects
     for_each(
         scores,
         function(row) {
+            // convert each row to an array for now
             var rowarr = [];
             for_each(keys(row), function(i) { rowarr.push(row[i]); });
             $("#scores").append("<div>"+rowarr.join(", ")+"</div>");
@@ -21,4 +24,36 @@ var display_scores = function(data) {
 // gets and displays scores
 var do_scores = function() {
     get_scores(display_scores);
+};
+
+// inserts a score into the db
+// score_obj:
+//  - score: int score
+//  - level: mutation level
+//  - userid: unique id for the user (from fb?)
+var post_score = function(score_obj, callback) {
+    $.post("post_score.php", score_obj, callback);
+};
+
+// testing function to use with form
+var submit_score = function() {
+    var form = document.forms[0];
+    var score = form.elements[0].value; 
+    var level = form.elements[1].value; 
+    var userid = form.elements[2].value; 
+
+    var score_obj = {
+       score: parseInt(score),
+       level: parseInt(level),
+       userid: userid
+    };
+    console.log(score_obj);
+
+    post_score(
+        score_obj,
+        function(data) {
+            console.log(data);
+            do_scores();
+        }
+    );
 };
