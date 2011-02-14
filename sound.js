a3d1c130fa06b9170c23c50490844a19b6c59519
vjsingh <varun101@gmail.com>
2011-02-14 13:32:54 -0500

    testing

--- sound.js
+++ sound.js
@@ -200,7 +200,7 @@ var sound_manager = function() {
 
 	obj.load_sounds = function() {
 		// init all bg music
-		var all_supplied = "mp3, ogg";
+		var all_supplied = "ogg, mp3";
         var init_jplayer = function(name, mp3name, oggname, should_loop) {
 			$(name).jPlayer( {
 				swfPath : the_swf_path,
